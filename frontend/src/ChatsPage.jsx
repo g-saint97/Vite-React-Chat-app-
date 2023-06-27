1cd183f62b484bcf99082bfc367d4673d9674df1
George Saint Hilaire <georgesainthilaire@Georges-iMac.local>
2023-06-27 14:58:20 -0400

    First commit

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="b42bc2db-e4b7-4150-8039-f3adf370c7f3"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100vh" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
